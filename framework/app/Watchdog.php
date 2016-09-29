a011375e6d6afe79aae5d88e1494343c568629b1
Neo Ighodaro <neo@creativitykills.co>
2016-09-29 23:26:54 +0100

    Added custom extension for Illuminate application.

---
+++ framework/app/Watchdog.php
@@ -0,0 +1,29 @@
+<?php
+namespace App;
+
+use Illuminate\Foundation\Application as IlluminateApplication;
+
+class Watchdog extends IlluminateApplication {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNamespace()
+    {
+        if (! is_null($this->namespace)) {
+            return $this->namespace;
+        }
+
+        $composer = json_decode(file_get_contents(base_path('../composer.json')), true);
+
+        foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
+            foreach ((array) $path as $pathChoice) {
+                if (realpath(app_path()) == realpath(base_path().'/../'.$pathChoice)) {
+                    return $this->namespace = $namespace;
+                }
+            }
+        }
+
+        throw new RuntimeException('Unable to detect application namespace.');
+    }
+}
--- framework/bootstrap/app.php
+++ framework/bootstrap/app.php
@@ -11,7 +11,7 @@
 |
 */
 
-$app = new Illuminate\Foundation\Application(
+$app = new App\Watchdog(
     realpath(__DIR__.'/../')
 );
 
