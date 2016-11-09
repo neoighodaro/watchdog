c7aeb6badcfa90985c89ade9bc784a005f173359
Neo Ighodaro <neo@creativitykills.co>
2016-11-09 16:55:56 +0100

    COmpleted slack notifications

--- content/plugins/Slack/Events/WatchdogCheckFailed.php
+++ content/plugins/Slack/Events/WatchdogCheckFailed.php
@@ -2,10 +2,17 @@
 
 namespace App\Plugin\Slack\Events;
 
+use App\Option;
+use Carbon\Carbon;
 use App\Plugin\Slack;
 use App\Events\WatchdogCheckFailed;
 
 class WatchdogCheckFailed {
+
+    /**
+     * Slack notification posting intervals.
+     */
+    const INTERVALS = 3;
 
     /**
      * Event instance.
@@ -61,9 +68,10 @@ class WatchdogCheckFailed {
      */
     protected function brokenMoreThanOnceEvent($service, $description)
     {
-        $errorMessage = "Seems there's something up with \"{$service->name}\".\n{$description}";
+        $errorMessage  = "Seems there's something up with \"{$service->name} ({$service->url})";
+        $errorMessage .= "\n{$description}";
 
-        return $this->postNotification("_Woof!!! Watchdog encountered an issue connecting to one of the services..._", [
+        return $this->postNotification("_Woof!!! Watchdog encountered an issue connecting to one of the services... <@channel>_", [
             'color' => 'danger',
             'title' => 'Houston, we have a problem!',
             'text' => $errorMessage,
@@ -88,14 +96,45 @@ class WatchdogCheckFailed {
      *
      * @param  string $msg
      * @param  array  $attachments
-     * @return null
+     * @return null|array
      */
     protected function postNotification($msg, array $attachments = [])
     {
-        return (new Slack\Notification)->postChatMessage($msg, [
-            'channel'     => env('SLACK_POST_CHANNEL'),
-            'icon_url'    => asset('img/watchdog@2x.png'),
-            'attachments' => json_encode([$attachments]),
-        ]);
+        if ($this->shouldPostNotification()) {
+            $post = (new Slack\Notification)->postChatMessage($msg, [
+                'channel'     => env('SLACK_POST_CHANNEL'),
+                'icon_url'    => asset('img/watchdog@2x.png'),
+                'attachments' => json_encode([$attachments]),
+            ]);
+
+            if ($post['status'] === 'success') {
+                $timestamp = (int) strtotime(Carbon::now()->toDateTimeString());
+
+                (new Option)->setOption('_slack_last_posted', $timestamp);
+            }
+
+            return $post;
+        }
+
+        return null;
+    }
+
+    /**
+     * Check if Slack should post a notification. To avoid spamming the channel you
+     * should only post once every x minutes.
+     *
+     * @return boolean
+     */
+    protected function shouldPostNotification()
+    {
+        if ($timestamp = (new Option)->getOption('_slack_last_posted')) {
+            $lastPostedTime = Carbon::createFromTimestamp($timestamp);
+
+            $currentTime = Carbon::now();
+
+            return $lastPostedTime->diffInMinutes($currentTime) >= static::INTERVALS;
+        }
+
+        return true;
     }
 }
---
+++ framework/app/Option.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected $fillable = ['name', 'value'];
+
+    /**
+     * Get an option from the database.
+     *
+     * @param  string  $option
+     * @param  mixed $default
+     * @return mixed
+     */
+    public function getOption($option, $default = false)
+    {
+        if ( ! $option = static::whereOptionName(strtolower($option))->first()) {
+            return $default;
+        }
+
+        return $this->isJsonEncoded($option->value)
+            ? json_decode($option->value, true)
+            : $option->value;
+    }
+
+    /**
+     * Set an option.
+     *
+     * @param  string $option
+     * @param  mixed  $value
+     * @return mixed
+     */
+    public function setOption($option, $value)
+    {
+        $value = $this->shouldJsonEncode($value) ? json_encode($value) : $value;
+
+        return $this->updateOrCreate(
+            ['name'  => strtolower($option)],
+            ['value' => $value]
+        );
+    }
+
+    /**
+     * Checks if the string is json encoded.
+     *
+     * @param  string  $string
+     * @return boolean
+     */
+    protected function isJsonEncoded($string)
+    {
+        if ( ! is_string($string) OR (strpos($string, '{') !== 0 AND strpos($string, '[') !== 0)) {
+            return false;
+        }
+
+        json_decode($string);
+
+        return (json_last_error() == JSON_ERROR_NONE);
+    }
+
+    /**
+     * Check if the value should be encoded.
+     *
+     * @param  string $string
+     * @return boolean
+     */
+    protected function shouldJsonEncode($string)
+    {
+        return ! is_string($string) && ! is_numeric($string);
+    }
+}
